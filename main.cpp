137e991ba07ba28ffa0b631185d1371fe023199d
shar3nda <vyavya2003@yandex.ru>
2022-12-16 10:46:44 +0300

    change standard to c++20; change printf to dbgout marco

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@
 #include <queue>
 #include <sstream>
 #include <iomanip>
+#include <syncstream>
 
 std::string tm() {
-    // get a precise timestamp as a string
     const auto now = std::chrono::system_clock::now();
     const auto now_as_time_t = std::chrono::system_clock::to_time_t(now);
     const auto now_ms =
         std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
     std::stringstream now_ss;
-    now_ss << std::put_time(std::localtime(&now_as_time_t), "[%T")
-          << '.' << std::setfill('0') << std::setw(3) << now_ms.count() << "] ";
+    now_ss << std::put_time(std::localtime(&now_as_time_t), "[%T") << '.' << std::setfill('0')
+           << std::setw(3) << now_ms.count() << "] ";
     return now_ss.str();
 }
+
+#define dbgout std::osyncstream(std::cout) << tm()
 
 enum task_type {
     WRONG, OK, TO_CHECK
@@ -46,15 +48,9 @@ void *doWork(void *void_worker_id) {
     int self_task_id = 1;
 
     while (true) {
-        printf("%sПрограммист %d решает задачу №%d\n",
-               tm().c_str(),
-               worker_id,
-               self_task_id);
+        dbgout << "Программист " << worker_id << " решает задачу №" << self_task_id << "\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 3000 + 1000));
-        printf("%sПрограммист %d решил задачу №%d\n",
-               tm().c_str(),
-               worker_id,
-               self_task_id);
+        dbgout << "Программист " << worker_id << " решил задачу №" << self_task_id << "\n";
         // в очередь прогера to_id добавить задачу
         int sent_to_id = rand() % 2;
         if (worker_id == 0 || worker_id == 1 && sent_to_id == 1) {
@@ -64,11 +60,8 @@ void *doWork(void *void_worker_id) {
         tasks[sent_to_id].emplace(TO_CHECK, worker_id, self_task_id);
         pthread_mutex_unlock(&mutex);
 
-        printf("%sПрограммист %d отправил задачу №%d на ревью программисту %d\n",
-               tm().c_str(),
-               worker_id,
-               self_task_id,
-               sent_to_id);
+        dbgout << "Программист " << worker_id << " отправил задачу №" << self_task_id
+               << " на ревью программисту " << sent_to_id << "\n";
 
         while (true) {
             if (!tasks[worker_id].empty()) {
@@ -81,46 +74,32 @@ void *doWork(void *void_worker_id) {
                     ++self_task_id;
                     break;
                 } else if (task.type == WRONG) {
-                    printf("%sПрограммист %d перерешивает задачу №%d\n",
-                           tm().c_str(),
-                           worker_id,
-                           self_task_id);
+                    dbgout << "Программист " << worker_id << " перерешивает задачу №"
+                           << self_task_id << "\n";
                     std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 3000 + 1000));
                     pthread_mutex_lock(&mutex);
                     tasks[task.from_id].push(Task(TO_CHECK, worker_id, self_task_id));
-                    printf("%sПрограммист %d повторно отправил задачу №%d на ревью программисту %d\n",
-                           tm().c_str(),
-                           worker_id,
-                           self_task_id,
-                           task.from_id);
                     pthread_mutex_unlock(&mutex);
+                    dbgout << "Программист " << worker_id << " повторно отправил задачу №"
+                           << self_task_id << " на ревью программисту " << task.from_id << "\n";
                 } else {
-                    printf("%sПрограммист %d проверяет задачу №%d программиста %d\n",
-                           tm().c_str(),
-                           worker_id,
-                           task.task_id,
-                           task.from_id);
+                    dbgout << "Программист " << worker_id << " проверяет задачу №" << task.task_id
+                           << " программиста " << task.from_id << "\n";
                     std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 10000 + 1000));
                     int is_correct = rand() % 2;
-                    printf("%sПрограммист %d проверил задачу №%d программиста %d. Вердикт: %s\n",
-                           tm().c_str(),
-                           worker_id,
-                           task.task_id,
-                           task.from_id,
-                           is_correct ? "OK" : "WRONG");
+                    dbgout << "Программист " << worker_id << " проверил задачу №" << task.task_id
+                           << " программиста " << task.from_id << ". Вердикт: "
+                           << (is_correct ? "OK" : "WRONG") << "\n";
                     pthread_mutex_lock(&mutex);
                     tasks[task.from_id].push(Task(is_correct ? OK : WRONG,
                                                   worker_id,
                                                   self_task_id));
-                    if (!is_correct) {
-                        printf(
-                            "%sПрограммист %d отправил задачу №%d на переписывание программисту %d\n",
-                            tm().c_str(),
-                            worker_id,
-                            self_task_id,
-                            task.from_id);
-                    }
                     pthread_mutex_unlock(&mutex);
+                    if (!is_correct) {
+                        dbgout << "Программист " << worker_id << " отправил задачу №"
+                               << self_task_id << " на переписывание программисту " << task.from_id
+                               << "\n";
+                    }
                 }
             }
         }
